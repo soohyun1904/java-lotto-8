0ea5610aa831db1a5a2116c8597bba7b0c3fa45b
SooHyeon Lee <dalsu4594@naver.com>
2025-11-02 00:10:02 +0900

    test: IntegerParser 메소드 수정 및 추가에 따른 테스트 수정 및 추가
    - 쉼표마다 문자열을 나누는 기능 테스트
    - 정수값을 초과한 경우 테스트

--- src/test/java/lotto/util/IntegerParserTest.java
+++ src/test/java/lotto/util/IntegerParserTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import static lotto.exception.message.ErrorMessage.EMPTY_PURCHASE_AMOUNT;
-import static lotto.exception.message.ErrorMessage.INVALID_INTEGER_FORMAT;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+import static lotto.exception.message.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.*;
 
 class IntegerParserTest {
 
@@ -26,7 +27,7 @@ class IntegerParserTest {
         String input = "  ";
         assertThatThrownBy(() -> IntegerParser.parse(input))
                 .isInstanceOf(InputValidationException.class)
-                .hasMessage(EMPTY_PURCHASE_AMOUNT.getMessage());
+                .hasMessage(INVALID_EMPTY_INPUT.getMessage());
     }
 
     @ParameterizedTest
@@ -36,5 +37,49 @@ class IntegerParserTest {
         assertThatThrownBy(() -> IntegerParser.parse(input))
                 .isInstanceOf(InputValidationException.class)
                 .hasMessage(INVALID_INTEGER_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2147483648", "-2147483649"})
+    @DisplayName("정수 범위를 초과하면 예외를 발생시킨다.")
+    void throwExceptionWhenOutOfRange(String input){
+        assertThatThrownBy(() -> IntegerParser.parse(input))
+                .isInstanceOf(InputValidationException.class)
+                .hasMessage(INVALID_INTEGER_RANGE.getMessage());
+    }
+
+    @Test
+    @DisplayName("구분자로 구분된 정수 문자열을 리스트로 변환한다.")
+    void parseIntegersWithDelimiter(){
+        String input = "1,2,3,4,5,6";
+        List<Integer> integers = IntegerParser.parseWithDelimiter(input, ",");
+        assertThat(integers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {", ,2,34,5", ",12,32,12  ,32,12"})
+    @DisplayName("쉼표 사이에 빈 값이 있으면 예외처리한다.")
+    void throwExceptionWhenInputContainsEmptyValueBetweenCommas(String input) {
+        assertThatThrownBy(() -> IntegerParser.parseWithDelimiter(input, ","))
+                .isInstanceOf(InputValidationException.class)
+                .hasMessage(INVALID_EMPTY_TOKEN.getMessage());
+    }
+
+    @Test
+    @DisplayName("빈 입력을 받는 경우 예외처리한다.")
+    void throwExceptionWhenInputIsEmptyWithDelimiter(){
+        String input = "  ";
+        assertThatThrownBy(() -> IntegerParser.parse(input))
+                .isInstanceOf(InputValidationException.class)
+                .hasMessage(INVALID_EMPTY_INPUT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2147483648,3", "1,-2147483649,3"})
+    @DisplayName("구분자 사이에 정수 범위를 초과하는 값이 있으면 예외를 발생시킨다")
+    void throwExceptionWhenOutOfRangeBetweenDelimiters(String input){
+        assertThatThrownBy(() -> IntegerParser.parseWithDelimiter(input, ","))
+                .isInstanceOf(InputValidationException.class)
+                .hasMessage(INVALID_INTEGER_RANGE.getMessage());
     }
 }
