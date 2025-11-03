55f64ab73fa2361b56e605f1fe0b808b31a04b12
SooHyeon Lee <dalsu4594@naver.com>
2025-11-03 14:28:27 +0900

    refactor: printf로 구현되어 /n가 제대로 되지 않는 현상 발견 후 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,14 +8,12 @@ import java.util.List;
 
 public class OutputView {
     private final static String LOTTO_PURCHASE_MESSAGE = "%s개를 구매했습니다.";
-
     private static final String STATISTICS_HEADER = "당첨 통계";
     private static final String STATISTICS_SEPARATOR = "---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
     private static final String RANK_FORMAT = "%s - %d개";
 
     private record RankInfo(String name, String description) {}
-
     private static final List<RankInfo> RANK_INFOS = List.of(
             new RankInfo("FIFTH", "3개 일치 (5,000원)"),
             new RankInfo("FOURTH", "4개 일치 (50,000원)"),
@@ -25,7 +23,7 @@ public class OutputView {
     );
 
     public void printLottos(LottosDto lottos){
-        System.out.printf(String.format(LOTTO_PURCHASE_MESSAGE, lottos.count()));
+        System.out.println(String.format(LOTTO_PURCHASE_MESSAGE, lottos.count()));
         lottos.lottos().forEach(this::printLotto);
         System.out.println();
     }
@@ -47,11 +45,11 @@ public class OutputView {
                 printRankStatistics(rank.name(), rank.description(), result)
         );
 
-        System.out.printf(String.format(RATE_OF_RETURN_MESSAGE, result.rateOfReturn()));
+        System.out.println(String.format(RATE_OF_RETURN_MESSAGE, result.rateOfReturn()));
     }
 
     private void printRankStatistics(String rank, String description, LottoResultDto result) {
         long count = result.getCountByRank(rank);
-        System.out.printf(String.format(RANK_FORMAT, description, count));
+        System.out.println(String.format(RANK_FORMAT, description, count));
     }
 }
